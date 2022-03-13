13de71b4e8f0ed18e22a307bdceaad16ec40c545
Wilciara <wilciara@gmail.com>
2022-03-12 22:19:22 -0300

    adicionando aluraflix top movies 2022

---
+++ script.js
@@ -0,0 +1,16 @@
+var listaFilmes = [
+    "https://geekblog.com.br/wp-content/uploads/2021/07/filmes-2022-14.jpg",
+    "https://trecobox.com.br/wp-content/uploads/2021/11/filmes-2022.jpg",
+    "https://tudoparahomens.com.br/wp-content/uploads/2020/10/Matrix-4.jpg",
+    "https://viciados.net/wp-content/uploads/2021/08/Grace-Randolph-talks-about-Hugh-Jackman-reprising-his-role-as-Wolverine-in-%E2%80%98Doctor-Strange-2-1-1024x576.jpg",
+    "https://viciados.net/wp-content/uploads/2022/01/Marvel-UCM-2022-Filmes-Series-Multiverso-Doutor-Estranho.webp",
+    "https://i0.wp.com/cidadedoheroi.com.br/wp-content/uploads/2021/11/filmes-2022-9.jpg?resize=693%2C405&ssl=1",
+    "https://portalnco.com.br/images/noticias/523/23a8f882ef77b1fbd4a4886b56fcbc61.jpg",
+    "https://lh3.googleusercontent.com/rdUxSrYFwzOk3PWBRyK7r0pbZpsu7hdw-g3bqRrEIZdyVtaCAaAptMGOKnbHf8veRcZXWgDXgHGr",
+    "https://s3-us-west-2.amazonaws.com/drupal-assets/atlanticcity/styles/card_396x280/s3-files/images-card/event/TopGun_Maverick_card.jpg?itok=Xqt_r6Vr"
+  ];
+  
+  for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src=" + listaFilmes[i] + ">");
+  }
+  
